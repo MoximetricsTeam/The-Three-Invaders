bc331ea086fb3b9520aec7e48eac4bb6a3e50408
cory <parravano.cory@gmail.com>
2020-08-26 04:35:51 -0700

    added book page

---
+++ src/constants/social.js
@@ -0,0 +1,31 @@
+import React from "react";
+import {
+  FaFacebookF,
+  FaTwitter,
+  FaInstagram,
+  FaLinkedinIn,
+  FaYoutube,
+} from "react-icons/fa";
+
+export default [
+  {
+    icon: <FaFacebookF />,
+    url: "https://facebook.com",
+  },
+  {
+    icon: <FaTwitter />,
+    url: "https://twitter.com",
+  },
+  {
+    icon: <FaInstagram />,
+    url: "https://instagrame.com",
+  },
+  {
+    icon: <FaLinkedinIn />,
+    url: "https://linkedin.com",
+  },
+  {
+    icon: <FaYoutube />,
+    url: "https://youtube.com",
+  },
+];
