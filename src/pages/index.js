8de2421e4d98410ffd15e5af324346718373df6d
Tydoo <tyler@moximetrics.com>
2020-09-21 16:00:41 -0500

    more casing changes

--- src/pages/index.js
+++ src/pages/index.js
@@ -1,19 +1,18 @@
-import React from "react";
-import { Link } from "gatsby";
-
-import Layout from "../components/layout";
-import HeroHome from "../components/home/HeroHome";
-import SEO from "../components/seo";
-import NavbarHome from "../components/home/NavbarHome";
-import BookSummery from "../components/home/BookSummery";
-import BuyOptions from "../components/BuyOptions";
-import Author from "../components/home/Author";
-import Quote from "../components/Quote";
-import Video from "../components/Video";
+import React from 'react';
+import { Link } from 'gatsby';
+import Layout from '../components/layout';
+import HeroHome from '../components/home/HeroHome';
+import SEO from '../components/seo';
+import NavbarHome from '../components/home/NavbarHome';
+import BookSummery from '../components/home/BookSummery';
+import BuyOptions from '../components/BuyOptions';
+import Author from '../components/home/Author';
+import Quote from '../components/Quote';
+import Video from '../components/Video';
 
 const IndexPage = () => (
   <Layout>
-    <SEO title="Home" />
+    <SEO title='Home' />
     <HeroHome />
     <NavbarHome />
     <BookSummery />
